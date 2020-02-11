531f264f2cbafba1f41d2c85605249c3710d142e
Chris Johnson <chrisjohnson00@hotmail.com>
2020-02-10 21:54:05 -0800

    Added tests for plex download url Updated pipeline to run the tests

---
+++ awx_proxy/tests/test_plex.py
@@ -0,0 +1,12 @@
+from awx_proxy.models import PlexDownloads
+
+
+def test_get_latest_version_from_plex():
+    """
+    This tests the download url to ensure it gives a proper response
+    If this fails, check the `plex_download_url` for valid-ness, it may have changed
+    """
+    plex = PlexDownloads()
+    plex.get_latest_version_from_plex()
+    latest_version = plex.get_latest_version()
+    assert '1' in latest_version

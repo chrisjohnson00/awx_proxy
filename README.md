c350303e8b7e6239c937785b09308e2016b4d6b4
Chris Johnson <chrisjohnson00@hotmail.com>
2020-02-13 21:07:16 -0800

    CS Fixes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/43cccec31d7b4058a8e29246a9be5a23)](https://www.codacy.com/manual/chrisjohnson00/awx_proxy?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=chrisjohnson00/awx_proxy&amp;utm_campaign=Badge_Grade)
+
 Run for dev
 
     FLASK_APP=app.py FLASK_ENV=development flask run
@@ -9,6 +11,5 @@ Run in docker
 Build the docker
 
     docker build . -t awx_proxy
-
 
 Update `instance/config.py` with your values for your testing.
--- awx_proxy/models.py
+++ awx_proxy/models.py
@@ -1,5 +1,5 @@
 """
-This file contains all the models for the awx proxy
+This file contains all the models for the awx proxy.
 """
 import json
 import requests
@@ -8,16 +8,19 @@ import time
 
 class PlexDownloads:
     """
-    This class handles the loading of raw json, and returns the plex version for download
+    PlexDownloads handles the loading of raw json, and returns the plex version for download.
     """
 
     def __init__(self):
+        """Constructor for PlexDownloads."""
         self.json = None
 
     def get_latest_version_from_plex(self):
+        """Fetches a json payload from plex's site."""
         plex_download_url = "https://plex.tv/api/downloads/5.json?channel=plexpass&cb={}".format(int(time.time()))
         r = requests.get(plex_download_url)
         self.json = json.loads(r.text)
 
     def get_latest_version(self):
+        """Returns the latest version from the json payload."""
         return self.json['computer']['Linux']['version']
